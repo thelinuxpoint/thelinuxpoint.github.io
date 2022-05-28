90eed775265f0bf3bcc66a9a8af6ccf87939e0ab
Prakash <choudharyprakash066@gmail.com>
2022-05-28 09:34:38 +0530

    updates

--- src/Content/Web/sql.js
+++ src/Content/Web/sql.js
@@ -12,23 +12,50 @@ function Author(){
                      var x = document.createElement('a');
                      x.href="https://www.youtube.com/channel/UCVYclxop0utX10SW0kOK9Ig";
                      x.click();}}>
-                <img src="youtube.dark.min.svg" alt="youtube" style={{'height':'30px','width':'30px'}}/>
+                <img src="https://thelinuxpoint.github.io/youtube.dark.min.svg" alt="youtube" style={{'height':'30px','width':'30px'}}/>
             </div>
             <div draggable={false} className="" title="Visit Github" onClick={function(){
                     var x = document.createElement('a');
                      x.href="https://github.com/thelinuxpoint";
                      x.click();}}>
-                <img src="github.dark.min.svg" alt="github" style={{'height':'30px','width':'30px'}}/>
+                <img src="https://thelinuxpoint.github.io/github.dark.min.svg" alt="github" style={{'height':'30px','width':'30px'}}/>
             </div>
         </div>
     );
+}
+function slideIN(){
+	if(window.innerHeight>window.innerWidth){
+		document.getElementsByClassName('flap-sql')[0].style.left="-300px";
+		if(document.getElementById("hambure")){
+                	let c = document.getElementById("hambure");
+                	c.id="hamburx";
+
+            	}else if(document.getElementById("hamburx")){
+                	let c = document.getElementById("hamburx");
+                	c.id="hambure";
+            	}
+	}
+}
+function Navigator(props){
+	return(
+		<>
+			<div className="pg-nav put-in-side-code">
+				<div className="pg-nav-in">
+					<NavLink to={props.left} className="descr" ><button id="first-btn">❱</button></NavLink>
+				</div>
+				<div className="pg-nav-in">
+					<NavLink to={props.right} className="descr" ><button>❱</button></NavLink>
+				</div>
+			</div>
+		</>
+	);
 }
 function FlapSql(props){
 	return(
 		<>
 		<div className="flap-sql">
 			<div className="display">
-				<img src="sql.png" style={{width:"200px",height:"150px"}}/>
+				<img src="https://thelinuxpoint.github.io/sql.png" style={{width:"200px",height:"150px"}}/>
 			</div>			
 			<hr/>
 			<div className="topic">
@@ -104,8 +131,8 @@ function SqlIntro(props){
 			<Shell desc="tlp:cyan sudo tlp:cyan apt-get install mysql-server"/>
 			<Mark desc=" This installs the package for the MySQL server, as well as the packages for the client and for the database common files. tlp:br During the installation, you are asked to supply a password for the root user for your MySQL installation. The MySQL server is started automatically after installation. You can check the status of the MySQL server with the following command: "/>
 			<Shell desc="tlp:cyan systemctl mysql status"/>
-
-
+			<br/>
+			<Navigator left="" right=""/>
 
 		</>
 	);
@@ -133,7 +160,8 @@ function SqlCreate(props){
 
 			<Table head="Database" colsp={1} desc="-   |"/>
 			<Mark desc="tlp:cyan Note : Be careful before dropping a database. Deleting a database will result in loss of complete information stored in the database!"/>
-
+<br/>
+			<Navigator left="" right=""/>
 
 		</>
 	);
